229cd26c72fe7bed3cec65e9150c5a52305ad3eb
Julian Nguyen <huutrong.160705@gmail.com>
2026-03-06 21:04:54 +0700

    initial commit

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Frontend đã chạy thành công!
-      </h1>
-    </main>
-  );
-}
